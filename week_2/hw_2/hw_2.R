fc69b25511ed598c010f4aa84de6fe0554ef10d9
PeterLiang <peter30616@gmail.com>
2018-09-29 01:26:43 +0800

    add date

--- week_2/hw_2/hw_2.R
+++ week_2/hw_2/hw_2.R
@@ -1,9 +1,19 @@
 library(rvest)
 library(httr)
+library(tmcn)
 
-url <- read_html("http://www.op.gg/summoner/userName=hide+on+bush", encoding = 'utf-8')
+url <- read_html("http://tw.op.gg/statistics/ajax2/champion/", encoding = 'utf-8')
 
-opgg.selected <- html_nodes(url, ".Cell , .KDA , .Cell a , .tablesorter-headerUnSorted")
-opgg.selected.content <- html_text(opgg.selected)
+champion <- html_nodes(url, ".ChampionName") %>% html_text(trim = T)
+rate <- html_nodes(url, ".ChampionName+ .Cell .Value") %>% html_text(trim = T)
+KDA <- html_nodes(url, ".Ratio") %>% html_text(trim = T)
+Gold <- html_nodes(url, ".KDARatio~ .Cell+ .Cell") %>% html_text(trim = T)
+CS <- html_nodes(url, ".KDARatio+ .Cell") %>% html_text(trim = T)
 
-opgg.selected.content
+champion.time <- Sys.time() %>% format('%a %b %d %X %Y')
+
+data <- data.frame(英雄 = champion,勝率 = rate, KDA率 = KDA, CS數 = CS, 金錢 = Gold, 日期 = champion.time)
+
+data
+
+write.table(data, file = "LoL 各英雄出場調查", sep = "  ")
