a101ea7c65e379f5620fe00ee9283e99a661f4fe
PeterLiang <peter30616@gmail.com>
2018-09-29 02:45:28 +0800

    fixed

--- week_2/hw_2/爬蟲練習/Rvest_Practice_2.R
+++ week_2/hw_2/爬蟲練習/Rvest_Practice_2.R
@@ -5,6 +5,7 @@ page.source <- read_html("https://en.wikipedia.org/wiki/R_(programming_language)
 version.block <- html_nodes(page.source, ".wikitable th+ td , th:nth-child(2) , .wikitable th:nth-child(1)")
 head(version.block)
 content <- html_text(version.block)
+content
 head(content)
 
 
