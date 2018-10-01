08fd112739658b7e94f72d2663dc5477e3414d9c
PeterLiang <peter30616@gmail.com>
2018-10-01 09:10:36 +0800

    complete hw_2

--- week_2/hw_2/hw_2.R
+++ week_2/hw_2/hw_2.R
@@ -2,18 +2,46 @@ library(rvest)
 library(httr)
 library(tmcn)
 
-url <- read_html("http://tw.op.gg/statistics/ajax2/champion/", encoding = 'utf-8')
+#找到目標 url
 
-champion <- html_nodes(url, ".ChampionName") %>% html_text(trim = T)
-rate <- html_nodes(url, ".ChampionName+ .Cell .Value") %>% html_text(trim = T)
-KDA <- html_nodes(url, ".Ratio") %>% html_text(trim = T)
-Gold <- html_nodes(url, ".KDARatio~ .Cell+ .Cell") %>% html_text(trim = T)
-CS <- html_nodes(url, ".KDARatio+ .Cell") %>% html_text(trim = T)
+url.tw <- read_html("http://tw.op.gg/statistics/ajax2/champion/")
+
+#利用 CSS selectors 切割出所需資料
+
+champion.tw <- html_nodes(url.tw, ".ChampionName") %>% html_text(trim = T)
+rate.tw <- html_nodes(url.tw, ".ChampionName+ .Cell .Value") %>% html_text(trim = T)
+KDA.tw <- html_nodes(url.tw, ".Ratio") %>% html_text(trim = T)
+Gold.tw <- html_nodes(url.tw, ".KDARatio~ .Cell+ .Cell") %>% html_text(trim = T)
+CS.tw <- html_nodes(url.tw, ".KDARatio+ .Cell") %>% html_text(trim = T)
+
+# 記錄索取資料時間
 
 champion.time <- Sys.time() %>% format('%a %b %d %X %Y')
 
-data <- data.frame(英雄 = champion,勝率 = rate, KDA率 = KDA, CS數 = CS, 金錢 = Gold, 日期 = champion.time)
+#匯出成表格
 
-data
+data.tw <- data.frame(英雄 = champion.tw,勝率 = rate.tw, KDA率 = KDA.tw, CS數 = CS.tw, 金錢 = Gold.tw, 日期 = champion.time)
 
-write.table(data, file = "LoL 各英雄出場調查（台灣）", sep = "  ")
+data.tw
+
+#寫出 csv 檔紀錄
+
+write.table(data.tw, file = "LoL 各英雄出場調查（台灣）_2", sep = "  ")
+
+#找尋到韓國伺服器重新做一次
+
+url.kr <- read_html("http://www.op.gg/statistics/ajax2/champion/", encoding = 'utf-8')
+
+champion.kr <- html_nodes(url.kr, ".ChampionName") %>% html_text(trim = T)
+rate.kr <- html_nodes(url.kr, ".ChampionName+ .Cell .Value") %>% html_text(trim = T)
+KDA.kr <- html_nodes(url.kr, ".Ratio") %>% html_text(trim = T)
+Gold.kr <- html_nodes(url.kr, ".KDARatio~ .Cell+ .Cell") %>% html_text(trim = T)
+CS.kr <- html_nodes(url.kr, ".KDARatio+ .Cell") %>% html_text(trim = T)
+
+data.kr <- data.frame(英雄 = champion.kr,勝率 = rate.kr, KDA率 = KDA.kr, CS數 = CS.kr, 金錢 = Gold.kr, 日期 = champion.time)
+
+#寫出 csv 檔紀錄
+data.kr
+
+write.table(data.kr, file = "LoL 各英雄出場調查（韓國）_2", sep = "  ")
+
