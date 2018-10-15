d0236be1c48a978a86948477c3c7bb2b35c32876
PeterLiang <peter30616@gmail.com>
2018-10-16 03:37:14 +0800

    hw_4

--- week_2/hw_2/hw_2_OOXX.R
+++ week_2/hw_2/hw_2_OOXX.R
@@ -2,12 +2,14 @@ drawboard <- function(x){
   cat(x[1],"|", x[2], "|", x[3],"\n")
   cat("_________\n")
   cat(x[4],"|", x[5], "|", x[6],"\n")
-  cat("_________\n")blank
+  cat("_________\n")
   cat(x[7],"|", x[8], "|", x[9],"\n")
 }
 
 blank = rep(" ", each = 9)
 blank
+
+drawboard(blank)
 
 for (i in 1:9){
   cat("Round", i,"\n")
@@ -15,8 +17,11 @@ for (i in 1:9){
     print("Now is A's turn!")
     print("Player A input(1:9): ")
     a <- readline()
+    if (a <= 9 && a >= 1)
+      
+    else
+      print("Invalid input! Please re-enter!")
   }
-  
 }
 
 if (blank[1] == blank[2] == blank[3] || blank[4] == blank[5] == blank[6] ||
